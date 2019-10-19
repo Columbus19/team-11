8a063331dbc7082b9dc7f6af8fe2efd0ded9fd13
Babacar Ndoye <babacarndoye13@gmail.com>
2019-10-19 00:24:48 -0400

    Schema

--- schema_sqlite.sql
+++ schema_sqlite.sql
@@ -1,13 +1,24 @@
-CREATE TABLE `users` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `registered_at` TIMESTAMP NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now', 'localtime')),
-  `last_login` TIMESTAMP NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now', 'localtime')),
-  `username` VARCHAR(255),
-  `password` VARCHAR(200),
-  `email` VARCHAR(200)
+CREATE TABLE users (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  registered_at TIMESTAMP NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now', 'localtime')),
+  last_login TIMESTAMP NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now', 'localtime')),
+  username VARCHAR(255),
+  password VARCHAR(200),
+  email VARCHAR(200)
 );
 
 CREATE TRIGGER `triggerUserLogin` AFTER UPDATE ON `users`
 BEGIN
    update `users` SET `last_login` = (strftime('%Y-%m-%d %H:%M:%S', 'now', 'localtime')) WHERE id = NEW.id;
 END;
+
+CREATE TABLE debt(
+  date VARCHAR(20),
+  location VARCHAR(255),
+
+  -- amount DOUBLE,
+  -- balance DOUBLE,
+  -- id INTEGER,
+  FOREIGN_KEY(id), REFERENCES users(id)
+
+);
